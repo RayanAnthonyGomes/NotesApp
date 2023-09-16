5cbc2c7060b1f037944b83a7c2af6ac3170eadb8
DragNano <magirayan84@gmail.com>
2023-09-16 18:29:27 +0600

    now we can show previous sessions, added Enter Key Event

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".inputBox");
+function showNotes(){
+    notesContainer.innerHTML = localStorage.getItem("notes");  
+}
+showNotes();
+
+function updateStorage(){
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
 
 createBtn.addEventListener("click", () =>{
     let inputBox = document.createElement("p");
@@ -15,4 +23,18 @@ notesContainer.addEventListener("click", function(e){
     if (e.target.tagName === "IMG"){
         e.target.parentElement.remove();
     }   
+    else if(e.target.tagName === "P"){
+        notes = document.querySelectorAll(".inputBox");
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        })
+    }
+})
+document.addEventListener("keydown", event =>{
+    if(event.key === "Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
 })
